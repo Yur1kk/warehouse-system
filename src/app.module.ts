6d74821f4c825c33863090691c3532044e669fd9
Yurii Soroka <urasor04@gmail.com>
2024-11-03 16:01:06 +0200

    feat: connected new modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from 'prisma/prisma.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { AdminModule } from './auth/admin/admin.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, AdminModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
