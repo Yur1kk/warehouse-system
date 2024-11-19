a43567e3aa3690e729938d71fc17dfd5ab39307e
Yurii Soroka <urasor04@gmail.com>
2024-11-19 19:42:48 +0200

    feat: provided pagination and fixed previous bugs

--- src/resources/material-resource.controller.ts
+++ src/resources/material-resource.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Post, UseGuards, Body, Get, Param, Patch, Delete, Query, Request, ParseIntPipe } from '@nestjs/common';
+import { Controller, Post, UseGuards, Body, Get, Param, Patch, Delete, Request, ParseIntPipe, Query } from '@nestjs/common';
 import { MaterialResourceService } from './material-resource.service';
 import { CreateMaterialResourceDto } from './dto/create-material-resource.dto';
-import { MaterialResource, ResourceStatus, ResourceType } from '@prisma/client';
+import { MaterialResource } from '@prisma/client';
 import { JwtAuthGuard } from '../auth/strategies/jwt-auth.guard';
 
 @Controller('material-resources')
@@ -9,48 +9,56 @@ export class MaterialResourceController {
   constructor(private readonly materialResourceService: MaterialResourceService) {}
 
   @UseGuards(JwtAuthGuard)
-  @Post()
-  create(@Request() req, @Body() createMaterialResourceDto: CreateMaterialResourceDto): Promise<MaterialResource> {
-    const userId = req.user.sub; 
-    return this.materialResourceService.create(createMaterialResourceDto, userId);
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Get('filter')
-  filter(@Request() req, @Query('status') status: string, @Query('type') type: string): Promise<MaterialResource[]> {
-    const userId = req.user.sub; 
-    return this.materialResourceService.filterResources(userId, status as ResourceStatus, type as ResourceType);
-  }
-  
-  @UseGuards(JwtAuthGuard)
   @Get()
-  findAll(@Request() req): Promise<MaterialResource[]> {
-    const userId = req.user.sub; 
-    return this.materialResourceService.findAll(userId);
+  async findAll(
+    @Request() req,
+    @Query('status') status: string,
+    @Query('type') type: string,
+    @Query('page', ParseIntPipe) page = 1,
+    @Query('limit', ParseIntPipe) limit = 10,
+  ): Promise<{ items: MaterialResource[]; totalPages: number }> {
+    const userId = req.user.sub;
+    return this.materialResourceService.findAll(userId, status, type, page, limit);
   }
 
   @UseGuards(JwtAuthGuard)
   @Get(':id')
   findOne(
     @Request() req,
-    @Param('id', new ParseIntPipe()) id: number 
+    @Param('id', ParseIntPipe) id: number,
   ): Promise<MaterialResource | null> {
     const userId = req.user.sub;
     return this.materialResourceService.findOne(id, userId);
   }
-  
+
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  create(
+    @Request() req,
+    @Body() createMaterialResourceDto: CreateMaterialResourceDto,
+  ): Promise<MaterialResource> {
+    const userId = req.user.sub;
+    return this.materialResourceService.create(createMaterialResourceDto, userId);
+  }
 
   @UseGuards(JwtAuthGuard)
   @Patch(':id')
-  update(@Request() req, @Param('id') id: string, @Body() updateData: Partial<CreateMaterialResourceDto>): Promise<MaterialResource> {
-    const userId = req.user.sub; 
-    return this.materialResourceService.update(Number(id), updateData, userId);
+  update(
+    @Request() req,
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateData: Partial<CreateMaterialResourceDto>,
+  ): Promise<MaterialResource> {
+    const userId = req.user.sub;
+    return this.materialResourceService.update(id, updateData, userId);
   }
 
   @UseGuards(JwtAuthGuard)
   @Delete(':id')
-  remove(@Request() req, @Param('id') id: string): Promise<MaterialResource> {
-    const userId = req.user.sub; 
-    return this.materialResourceService.remove(Number(id), userId);
+  remove(
+    @Request() req,
+    @Param('id', ParseIntPipe) id: number,
+  ): Promise<MaterialResource> {
+    const userId = req.user.sub;
+    return this.materialResourceService.remove(id, userId);
   }
 }
